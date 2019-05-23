8b3dc4b42392d5ae9ebbdd0a5d99c5e680152ddf
shynome <shynome@gmail.com>
2019-05-24 00:24:41 +0800

    修复禅道 ldap 账户二次验证失败的问题

--- module/user/ext/model/identify.php
+++ module/user/ext/model/identify.php
@@ -16,9 +16,16 @@ public function identify($account, $password)
         	if (0 == strcmp('Success', $pass)) {
         		$user = $record;
         		$ip   = $this->server->remote_addr;
-	            $last = $this->server->request_time;
-	            $this->dao->update(TABLE_USER)->set('visits = visits + 1')->set('ip')->eq($ip)->set('last')->eq($last)->where('account')->eq($account)->exec();
-	            $user->last = date(DT_DATETIME1, $user->last);
+						$last = $this->server->request_time;
+						$this->dao->update(TABLE_USER)
+							->set('visits = visits + 1')
+							->set('ip')->eq($ip)
+							->set('ip')->eq($ip)
+							// 禅道有多处地方需要验证密码, 所以需要存储密码的 MD5 到禅道里以供验证
+							->set('password')->eq(md5($password))
+							->set('last')->eq($last)
+							->where('account')->eq($account)->exec();
+						$user->last = date(DT_DATETIME1, $user->last);
         	}
         }		
 		return $user;

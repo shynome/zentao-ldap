72b6bb421cac59e9cd9afc1b896c94e36938900b
shynome <shynome@gmail.com>
2019-05-24 01:49:51 +0800

    密码二次验证的时候不从数据库里取值即可完成验证，所以密码不再存放到数据库中，避免泄露

--- ldap/module/user/ext/model/identify.php
+++ ldap/module/user/ext/model/identify.php
@@ -17,14 +17,9 @@ public function identify($account, $password)
         		$user = $record;
         		$ip   = $this->server->remote_addr;
 						$last = $this->server->request_time;
-						$this->dao->update(TABLE_USER)
-							// 禅道有多处地方需要验证密码, 所以需要存储密码的 MD5 到禅道里以供验证
-							->set('password')->eq(md5($password))
-							->set('visits = visits + 1')
-							->set('ip')->eq($ip)
-							->set('ip')->eq($ip)
-							->set('last')->eq($last)
-							->where('account')->eq($account)->exec();
+						// 禅道有多处地方需要二次验证密码, 所以需要保存密码的 MD5 在 session 中以供后续验证
+						$user->password = md5($password);
+						$this->dao->update(TABLE_USER)->set('visits = visits + 1')->set('ip')->eq($ip)->set('ip')->eq($ip)->set('last')->eq($last)->where('account')->eq($account)->exec();
 						$user->last = date(DT_DATETIME1, $user->last);
 						
 						/* Create cycle todo in login. */

f367526cf1e849a0ca8472340ba93515c7199be5
shynome <shynome@gmail.com>
2019-05-24 01:02:00 +0800

    fix： 用以查找用户的变量是错误的

--- ldap/module/user/ext/model/identify.php
+++ ldap/module/user/ext/model/identify.php
@@ -11,18 +11,18 @@ public function identify($account, $password)
             ->fetch();
         if ($record) {
         	$ldap = $this->loadModel('ldap');
-        	$account = $this->config->ldap->uid.'='.$account.','.$this->config->ldap->baseDN;
-        	$pass = $ldap->identify($this->config->ldap->host, $account, $password);
+        	$ldap_account = $this->config->ldap->uid.'='.$account.','.$this->config->ldap->baseDN;
+        	$pass = $ldap->identify($this->config->ldap->host, $ldap_account, $password);
         	if (0 == strcmp('Success', $pass)) {
         		$user = $record;
         		$ip   = $this->server->remote_addr;
 						$last = $this->server->request_time;
 						$this->dao->update(TABLE_USER)
+							// 禅道有多处地方需要验证密码, 所以需要存储密码的 MD5 到禅道里以供验证
+							->set('password')->eq(md5($password))
 							->set('visits = visits + 1')
 							->set('ip')->eq($ip)
 							->set('ip')->eq($ip)
-							// 禅道有多处地方需要验证密码, 所以需要存储密码的 MD5 到禅道里以供验证
-							->set('password')->eq(md5($password))
 							->set('last')->eq($last)
 							->where('account')->eq($account)->exec();
 						$user->last = date(DT_DATETIME1, $user->last);

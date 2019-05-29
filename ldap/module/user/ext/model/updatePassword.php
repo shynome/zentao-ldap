1880c799a84a6c6fb895a2351ee34c4b3d1deaef
shynome <shynome@gmail.com>
2019-05-29 12:08:00 +0800

    禁止 ldap 用户修改账号密码

---
+++ ldap/module/user/ext/model/updatePassword.php
@@ -0,0 +1,9 @@
+<?php
+public function updatePassword($userID)
+{
+  if( $this->app->user->fromldap == true ){
+    dao::$errors['originalPassword'][] = "ldap 用户不能修改修改密码"
+    return false
+  }
+  return parent::updatePassword($userID);
+}

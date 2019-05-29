a7020a1367c6d7fa1187dd96199a66e1d2dcf381
shynome <shynome@gmail.com>
2019-05-29 15:42:28 +0800

    修正文本

--- ldap/module/user/ext/model/updatePassword.php
+++ ldap/module/user/ext/model/updatePassword.php
@@ -2,7 +2,7 @@
 public function updatePassword($userID)
 {
   if( $this->app->user->fromldap == true ){
-    dao::$errors['originalPassword'][] = "ldap 用户不能修改修改密码";
+    dao::$errors['originalPassword'][] = "ldap 用户不能修改密码";
     return false;
   }
   return parent::updatePassword($userID);
